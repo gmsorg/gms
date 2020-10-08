ebf7ac91241a1a7687dfb254c9eb2d5d62074da5
高建文 <cmdgjw@hotmail.com>
2020-10-08 15:16:37 +0800

    完善注释

--- protocol/message.go
+++ protocol/message.go
@@ -31,30 +31,51 @@ func (m *Message) SetExtLen(extLen uint32) {
 	m.ExtLen = extLen
 }
 
+/*
+GetExtLen 获取扩展数据的长度
+*/
 func (m *Message) GetExtLen() uint32 {
 	return m.ExtLen
 }
 
+/*
+SetExt 设置扩展数据
+*/
 func (m *Message) SetExt(ext []byte) {
 	m.Ext = ext
 }
 
+/*
+GetExt 获取扩展数据
+*/
 func (m *Message) GetExt() []byte {
 	return m.Ext
 }
 
+/*
+SetDataLen 设置主体数据段长度
+*/
 func (m *Message) SetDataLen(dataLen uint32) {
 	m.DataLen = dataLen
 }
 
+/*
+GetDataLen 获取主体数据段长度
+*/
 func (m *Message) GetDataLen() uint32 {
 	return m.DataLen
 }
 
+/*
+SetData 设置主体数据内容
+*/
 func (m *Message) SetData(data []byte) {
 	m.Data = data
 }
 
+/*
+GetData 获取主体数据内容
+*/
 func (m *Message) GetData() []byte {
 	return m.Data
 }
